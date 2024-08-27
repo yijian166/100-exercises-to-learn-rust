f4c885c87af40d7125aa046afd1c516e213ad6a1
yijian166 <iwcc@outlook.com>
2024-08-27 08:42:28 +0800

    feat: Basically finished.

--- exercises/08_futures/02_spawn/src/lib.rs
+++ exercises/08_futures/02_spawn/src/lib.rs
@@ -1,10 +1,26 @@
+
 use tokio::net::TcpListener;
 
 // TODO: write an echo server that accepts TCP connections on two listeners, concurrently.
 //  Multiple connections (on the same listeners) should be processed concurrently.
 //  The received data should be echoed back to the client.
 pub async fn echoes(first: TcpListener, second: TcpListener) -> Result<(), anyhow::Error> {
-    todo!()
+    let handle1 = tokio::spawn(echo(first));
+    let handle2 = tokio::spawn(echo(second));
+    handle1.await??;
+    handle2.await??;
+    Ok(())
+}
+
+async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
+    loop {
+        let (socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let (mut reader, mut writer) = socket.into_split();
+            tokio::io::copy(&mut reader, &mut writer).await?;
+            Ok::<_, anyhow::Error>(())
+        });
+    }
 }
 
 #[cfg(test)]
--- exercises/08_futures/03_runtime/src/lib.rs
+++ exercises/08_futures/03_runtime/src/lib.rs
@@ -2,6 +2,7 @@
 //  accept connections on both of them concurrently, and always reply clients by sending
 //  the `Display` representation of the `reply` argument as a response.
 use std::fmt::Display;
+use std::sync::Arc;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 
@@ -10,7 +11,19 @@ where
     // `T` cannot be cloned. How do you share it between the two server tasks?
     T: Display + Send + Sync + 'static,
 {
-    todo!()
+    let reply = Arc::new(reply);
+    let handle1 = tokio::spawn(_fixed_reply(first, Arc::clone(&reply)));
+    let handle2 = tokio::spawn(_fixed_reply(second, reply));
+    tokio::join!(handle1, handle2);
+}
+
+
+async fn _fixed_reply<T>(first: TcpListener, reply: Arc<T>) where T: Display + Send + Sync + 'static {
+    loop {
+        let (socket, _) = first.accept().await.unwrap();
+        let mut socket = socket;
+        socket.write_all(format!("{}", reply).as_bytes()).await.unwrap();
+    }
 }
 
 #[cfg(test)]
